d3ac80fc1b222ea098c0bfdcec7e69fdb9dfecac
Prudestock <joffrey.pruvost@gmail.com>
2024-04-04 00:46:23 +0200

    Empty file, for the moment

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-import csv
-import sqlite3
-
-
-
-# Create a database
-connection = sqlite3.connect("fds.db")
-cursor = connection.cursor()
-#cursor.execute("CREATE TABLE IF NOT EXISTS fds (number int, name varchar(255), synonyms varchar(255))")
-
-
-
-
-connection.close()
+# Currently empty
+# Should launch a Tkinter_App
